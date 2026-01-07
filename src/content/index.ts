e4dcbe97855e11d579cc49cf226eaa91a9f92fa4
Shashikanth Reddy Kolan <48406108+shashiirk@users.noreply.github.com>
2026-01-07 16:38:34 -0500

    chore: update personal info and resume (#2)
    * chore: update personal info and resume
    
    * chore: update deps

--- src/content/index.ts
+++ src/content/index.ts
@@ -2,9 +2,9 @@ import type { PersonalInfo, SocialMediaItem, WorkExperienceItem } from '@/types'
 
 export const PERSONAL_INFO: PersonalInfo = {
 	name: 'Shashikanth Reddy',
-	email: 'kolanshashii@gmail.com',
+	email: 'hello@shashikanth.dev',
 	role: 'Full-Stack Engineer',
-	about: `I'm a Software Engineer with over two years of experience in full-stack development, currently pursuing my Master's degree in Computer Science at <a href="https://www.kennesaw.edu" target="_blank" rel="nopener noreferrer" class="font-medium hover:text-indigo-400 hover:underline focus-visible:text-indigo-400">Kennesaw State University</a>. My journey into tech began with Brad Traversy's HTML & CSS crash course on YouTube, sparking my love for building things and turning into a passion for coding and full-stack development.
+	about: `I'm a Software Engineer with over three years of experience in full-stack development, currently pursuing my Master's degree in Computer Science at <a href="https://www.kennesaw.edu" target="_blank" rel="nopener noreferrer" class="font-medium hover:text-indigo-400 hover:underline focus-visible:text-indigo-400">Kennesaw State University</a>. My journey into tech began with Brad Traversy's HTML & CSS crash course on YouTube, sparking my love for building things and turning into a passion for coding and full-stack development.
 
 	Since then, I've worked in various environments, from startups to established companies, focusing on creating user-friendly applications that are attractive, easy to navigate, and efficient behind the scenes. I enjoy simplifying complex tasks to enhance the user experience. Proficient in languages like Java, JavaScript, and Python, I've also worked with frameworks like Spring Boot, React, and Next.js.
 
@@ -41,7 +41,7 @@ export const WORK_EXPERIENCES: WorkExperienceItem[] = [
 		url: 'https://wisdomsquare.net',
 		role: 'Programmer',
 		startDate: 'March 2024',
-		endDate: 'July 2024',
+		endDate: 'Present',
 		description:
 			'Improved the internal logging framework, ensuring sensitive data was masked and privacy standards were met. Automated the sending of physical mail through third-party services, which reduced manual work and ensured timely delivery. Also created a reminder system that kept agents informed about scheduled interactions, improving response times and overall customer engagement.',
 		technologies: ['Java', 'Spring Boot', 'PostgreSQL', 'AWS']
